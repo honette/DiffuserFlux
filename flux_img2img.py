d4d849ce0763c1223454b41501e3ae7e64638db1
NAVIT Y.KOJIMA <navit.ykojima@gmail.com>
2025-10-07 17:10:36 +0900

    flux_img2img

--- flux_img2img.py
+++ flux_img2img.py
@@ -7,12 +7,11 @@ import readline
 from huggingface_hub import login
 from diffusers import DiffusionPipeline
 from diffusers.utils import load_image
+from PIL import Image
 import torch
 
 login(token=os.environ["HF_TOKEN"])
 
-from diffusers import DiffusionPipeline
-import torch
 pipe = DiffusionPipeline.from_pretrained(
     "black-forest-labs/FLUX.1-Kontext-dev",
     torch_dtype=torch.bfloat16,
@@ -20,11 +19,10 @@ pipe = DiffusionPipeline.from_pretrained(
     low_cpu_mem_usage=True
 ).to("cuda")
 
-
 pipe = FluxKontextPipeline.from_pretrained("black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16)
 pipe.to("cuda")
 
-input_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png")
+input_image = Image.open("sample/person.png")
 
 while True:
     prompt = input("\nType prompts : ")

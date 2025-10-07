a835799e206af448dbecf40a6a27cde41f3af3e9
NAVIT Y.KOJIMA <navit.ykojima@gmail.com>
2025-10-07 16:41:08 +0900

    qwen

--- qwen_img2img.py
+++ qwen_img2img.py
@@ -5,26 +5,22 @@ os.environ["HF_HUB_CACHE"] = "/workspace/hf_cache"
 from datetime import datetime
 import readline
 from huggingface_hub import login
-from diffusers import DiffusionPipeline
-from diffusers.utils import load_image
+from diffusers import QwenImageEditPlusPipeline
 import torch
+from PIL import Image
 
 login(token=os.environ["HF_TOKEN"])
 
-from diffusers import DiffusionPipeline
-import torch
-pipe = DiffusionPipeline.from_pretrained(
-    "black-forest-labs/FLUX.1-Kontext-dev",
+pipe = QwenImageEditPlusPipeline.from_pretrained(
+    "Qwen/Qwen-Image-Edit-2509",
     torch_dtype=torch.bfloat16,
     cache_dir="/workspace/hf_cache",
     low_cpu_mem_usage=True
 ).to("cuda")
 
-
-pipe = FluxKontextPipeline.from_pretrained("black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16)
-pipe.to("cuda")
-
-input_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png")
+pipe.set_progress_bar_config(disable=None)
+image1 = Image.open("sample/person.png")
+image2 = Image.open("sample/dog.png")
 
 while True:
     prompt = input("\nType prompts : ")
@@ -33,14 +29,13 @@ while True:
         break
 
     print(f"Now Generating : {prompt}")
-    image = pipe(
-        image=input_image,
-        prompt=prompt,
-        guidance_scale=2.5
-    ).images[0]
+    image = pipe(prompt=prompt).images[0]
+
+    output_dir = "./tmp"
+    os.makedirs(output_dir, exist_ok=True) 
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"flux_img2img_{timestamp}.png"
+    filename = f"qwen_img2img_{timestamp}.png"
     save_path = os.path.join(output_dir, filename)
     image.save(save_path)
     print(f"Completed : {filename}")

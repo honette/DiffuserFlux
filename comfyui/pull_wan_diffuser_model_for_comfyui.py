f810d10b47cae1140bd783a352d273caf62050bc
NAVIT Y.KOJIMA <navit.ykojima@gmail.com>
2025-10-10 13:39:04 +0900

    ComfyUI - Wan2.2 14B FP8

--- comfyui/pull_wan_diffuser_model_for_comfyui.py
+++ comfyui/pull_wan_diffuser_model_for_comfyui.py
@@ -15,6 +15,10 @@ files = {
         "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors",
     "models/vae/wan_2.1_vae.safetensors":
         "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors",
+    "models/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors":
+        "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors",
+    "models/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors":
+        "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors",
 }
 
 # 確実にディレクトリ作成

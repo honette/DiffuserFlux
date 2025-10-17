0b9bf8121f3b9e119c97018932285c2eb0d87841
Matarou <slinky@honette.com>
2025-10-18 02:08:09 +0900

    ComfyUI - Wan2.2 14B FP8
    New LoRA 2

--- comfyui/download_wan_lora.py
+++ comfyui/download_wan_lora.py
@@ -12,10 +12,10 @@ gdown.download(url, output, quiet=False)
 
 # DR34ML4Y_I2V_14B_HIGH
 url = "https://drive.google.com/uc?id=1MA_YbTPghda0Np1LKRo3btXpzKx0RtMW"
-output = "/workspace/runpod-slim/ComfyUI/models/loras/lora_wan_pov_high.safetensors"
+output = "/workspace/runpod-slim/ComfyUI/models/loras/lora_wan_dr34ml4y_high.safetensors"
 gdown.download(url, output, quiet=False)
 
 # DR34ML4Y_I2V_14B_LOW
 url = "https://drive.google.com/uc?id=1bQZaHwmz1x7C5HqNh2EPz7C-WXeNd9rp"
-output = "/workspace/runpod-slim/ComfyUI/models/loras/lora_wan_pov_low.safetensors"
+output = "/workspace/runpod-slim/ComfyUI/models/loras/lora_wan_dr34ml4y_low.safetensors"
 gdown.download(url, output, quiet=False)
